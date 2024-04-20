f704595ceafc63bc92d3baed33098a0848d3ac90
Hygor Gonçalves Marques <127366511+HygorGM@users.noreply.github.com>
2024-04-20 19:23:16 -0300

    Container Header
    Criação do template da header

--- app.vue
+++ app.vue
@@ -13,17 +13,3 @@ useHead({
   </NuxtLayout>
 </template>
 
-<style>
-html,
-body,
-#__nuxt {
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-}
-
-html.dark {
-  background: #222;
-  color: white;
-}
-</style>
---
+++ components/container.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="max-w-7xl mx-auto px-6">
+
+    <slot />
+  </div>
+</template>
