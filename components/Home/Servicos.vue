601e7fd526757c10be5b0f9acf484784e3551b38
Hygor Gonçalves Marques <127366511+HygorGM@users.noreply.github.com>
2024-05-18 20:48:04 -0300

    Desenvolvimento do Sobre mim
    Adicionamento de img e textos

--- components/Home/Servicos.vue
+++ components/Home/Servicos.vue
@@ -1,9 +1,9 @@
-<template>
-    <div>
-        servicos
-    </div>
-</template>
-
 <script setup lang="ts">
 
 </script>
+
+<template>
+  <div>
+    servicos
+  </div>
+</template>
--- components/Home/SobreMim.vue
+++ components/Home/SobreMim.vue
@@ -1,9 +1,39 @@
-<template>
-    <div>
-        sobremim
-    </div>
-</template>
-
 <script setup lang="ts">
 
 </script>
+
+<template>
+  <Container>
+    <div class="bg-gradient-to-b">
+      <div class="grid grid-cols-2 m-24 mx-56 gap-13">
+        <div>
+          <NuxtImg class="w-12 w-full" src="EngCivil460.jpg" />
+        </div>
+        <div>
+          <div class="intem-start mb-8">
+            <h1 class="text-2xl">
+              SOBRE MIM
+            </h1>
+          </div>
+          <div class="text-left">
+            <p class="mb-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ratione
+              perspiciatis,
+              consectetur
+              dolorem a dolor dignissimos id.
+            </p>
+          </div>
+          <ul class="list-disc pl-6 text-left">
+            <li>Gerenciamento de obras</li>
+            <li>Projetos arquitetônicos</li>
+            <li>Projetos complementares (Hidraulico, eletrico, estrutural e incêndio)</li>
+            <li>Regularizações de imóveis</li>
+            <li>Desmembramento de lotes</li>
+            <li>Perícias e avaliações técnicas</li>
+            <li>Financiamento de lote e construção ART (Anotação de responsabilidade técnica)</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </Container>
+</template>
