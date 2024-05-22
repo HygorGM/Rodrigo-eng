8ba456a2aac76396e2fa3c74f5844bb10685c7ef
Hygor Gonçalves Marques <127366511+HygorGM@users.noreply.github.com>
2024-05-21 22:32:52 -0300

    alteracao da sequencias de links
    sobre mim agora vem primeiro

--- components/HeaderChildren.vue
+++ components/HeaderChildren.vue
@@ -2,13 +2,13 @@
   <Container>
     <div class="grid grid-flow-col justify-center gap-10 p-8">
       <NuxtLink to="#URL">
+        SOBRE MIM
+      </NuxtLink>
+      <NuxtLink to="#URL">
         SERVIÇOS
       </NuxtLink>
       <NuxtLink to="#URL">
         PROJETOS
-      </NuxtLink>
-      <NuxtLink to="#URL">
-        SOBRE MIM
       </NuxtLink>
       <NuxtLink to="#URL">
         PARCEIROS
